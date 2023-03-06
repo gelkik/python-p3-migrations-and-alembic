8ca78cdafbfd7d0a9a277aca028b7e3ca797da5f
George Elkik <gelkik@gmail.com>
2023-03-06 10:07:27 -0500

    final

---
+++ lib/migrations/versions/fd5d84268431_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: fd5d84268431
+Revises: 
+Create Date: 2023-03-06 09:49:52.176341
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fd5d84268431'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
